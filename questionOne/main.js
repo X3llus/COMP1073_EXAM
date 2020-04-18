906f9634c31056315521e483708031cb57737d85
Braden Coates <xellus@pop-os.localdomain>
2020-04-18 13:24:21 -0400

    q1 finished

--- questionOne/main.js
+++ questionOne/main.js
@@ -1,14 +1,16 @@
 // JavaScript Document
 
-let main = document.querySelectorAll('main');
+let main = document.querySelector('main');
 let submitButton = document.querySelector('input[type="submit"]');
-let body = documant.querySelector('body'); 
+let body = document.querySelector('body'); 
+let input = document.querySelector("input[type=text]")
 
-submitButton.addEventListener('clickityclick', function(e) { 
-  let personName = input.value; 
+submitButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  let personName = input.value;
   let para = document.createElement('p'); 
   para.innerHTML = '<h3>Hey there ' + personName + '! Thanks for stopping by!</h3>'; 
-  main.appendChild(paragraph);
+  main.appendChild(para);
   body.setAttribute('class', 'meow'); 
   console.log('=^..^='); 
 }); 
